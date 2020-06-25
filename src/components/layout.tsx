1f41e0844b5346b7f22174a4690e62e338ababc4
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-06-25 17:19:01 +0800

    Enable smooth scroll behaviour in all browsers (#10)
    * Enable smooth scrolling in all browsers
    
    * Enable active nav style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,27 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent, ReactNode, useState } from "react";
 import { Box } from "@chakra-ui/core";
 
 import { NavBar } from "./navbar";
-import "./layout.css";
+
+if (typeof window !== "undefined") {
+  require("smooth-scroll")('a[href*="#"]');
+}
 
 interface LayoutComponentProps {
   children: ReactNode;
-  locations: string;
 }
 
 export const Layout: FunctionComponent<LayoutComponentProps> = ({
   children,
-  location,
-}) => (
-  <>
-    <NavBar location={location} />
-    <Box as="main" flexDirection="column">
-      {children}
-    </Box>
-  </>
-);
+}) => {
+  const [activeHash, setActiveHash] = useState("#home");
+
+  return (
+    <>
+      <NavBar activeHash={activeHash} setActiveHash={setActiveHash} />
+      <Box as="main" flexDirection="column">
+        {children}
+      </Box>
+    </>
+  );
+};
