ddf3d2d83fcebdc6eee97201a1da48d81b0894fa
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-06-25 18:39:19 +0800

    Add minor design changes (#12)
    * Change team sectin padding
    
    * Fix member card division inconsistency
    
    * Add logo on footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,18 @@
 import React, { FunctionComponent } from "react";
-import { Flex, Text, Link } from "@chakra-ui/core";
+import { Flex, Text, Link, Image } from "@chakra-ui/core";
+
+import hackSocLogo from "../../assets/images/hackSocLogo.png";
 
 export const Footer: FunctionComponent = () => (
   <Flex
+    direction="column"
     backgroundColor="text"
     justify="center"
+    align="center"
     textAlign="center"
     p={[5, 10, 10, 10]}
   >
+    <Image src={hackSocLogo} w={[10, 12, 16, 20]} h={[10, 12, 16, 20]} mb={5} />
     <Text color="primary">
       All rights reserved | Developed by&nbsp;
       <Link href="">HKU Hackathon Society</Link>
