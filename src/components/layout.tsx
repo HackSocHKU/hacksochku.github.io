8db6de103a6f7f860bafaa4a1426a2d209fb76b8
welvin21 <wewe2712@connect.hku.hk>
2020-06-25 16:37:44 +0800

    Enable smooth scrolling in all browsers

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,10 @@ import React, { FunctionComponent, ReactNode } from "react";
 import { Box } from "@chakra-ui/core";
 
 import { NavBar } from "./navbar";
-import "./layout.css";
+
+if (typeof window !== "undefined") {
+  require("smooth-scroll")('a[href*="#"]');
+}
 
 interface LayoutComponentProps {
   children: ReactNode;
