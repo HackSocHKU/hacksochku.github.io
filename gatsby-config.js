4d1edfcddffc25781a43af780bc243d0b67031b1
welvin21 <wewe2712@connect.hku.hk>
2020-06-25 17:53:26 +0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/images/hackSocLogo.png",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
