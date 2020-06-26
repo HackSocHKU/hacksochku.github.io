f1c632a0f1a594ce86243d05ef4e33c25708c656
welvin21 <wewe2712@connect.hku.hk>
2020-06-26 08:14:49 +0800

    Update team members info

--- src/components/team/listOfMembers.js
+++ src/components/team/listOfMembers.js
@@ -12,18 +12,25 @@ export const listOfMembers = [
     picture: siddharth,
     role: "President",
     linkedin: "https://www.linkedin.com/in/siddharthagarwal2608/",
+    github: "https://www.github.com/zweistein1326",
+    description:
+      "Siddharth is a junior studying Computer Science and Astronomy. He enjoys building new things that can bring about a change in the world. He is passionate about space and loves learning about new technologies. He believes in making the world a better and simpler place, one step at a time.",
   },
   {
     name: "Anubhav Garg",
     picture: anubhav,
     role: "Vice President",
     linkedin: "https://www.linkedin.com/in/anubhav-garg-9658a9a1/",
+    description:
+      "An ever smiling, super organised leader who is the first to arrive in every meeting! Determined, hard working team player passionate about doing the difficult things in an easy way.",
   },
   {
     name: "Tasnia Ishrar",
     picture: tasnia,
     role: "Head of External Relations",
     linkedin: "http://linkedin.com/in/tasnia-ishrar-r-013219173",
+    description:
+      "Tasnia is pursuing Bachelor of Engineering at HKU. She is an avid learner, and her hobbies include painting and coding.",
   },
   {
     name: "Welvin Bun",
@@ -32,24 +39,31 @@ export const listOfMembers = [
     linkedin: "https://www.linkedin.com/in/welvin-bun/",
     github: "https://github.com/welvin21",
     description:
-      "I enjoy taking complex problems and turning them into simple and efficient solutions using code.",
+      "A passionate progammer who enjoys taking complex problems and turning them into simple and efficient solutions using code.",
   },
   {
     name: "Alima Medetova",
     picture: alima,
     role: "Head of Event Management",
     linkedin: "http://linkedin.com/in/alima-medetova-27b589175",
+    description:
+      "Passionate, organised and detail-oriented. The key traits of our Events Manager, Alima. Loves to take challenges and does her best in commitments - something that makes her good at exploring new opportunities and getting things done.",
   },
   {
     name: "Aarushi Goyal",
     picture: aarushi,
     role: "Head of Corporate Relations",
     linkedin: "http://linkedin.com/in/aarushi-goyal-6b5226190",
+    description:
+      "An inquisitive problem solver, Aarushi is a social aficionado whose love for Technology is complemented by her dynamic problem solving abilities.",
   },
   {
     name: "Reynard Verill",
     picture: reynard,
     role: "Head of Marketing",
     linkedin: "https://www.linkedin.com/in/reynard-verill-234685196/",
+    github: "https://www.github.com/reyvb24",
+    description:
+      "I enjoy solving problems and working in a team. I am passionate about programming and financial markets.",
   },
 ];
