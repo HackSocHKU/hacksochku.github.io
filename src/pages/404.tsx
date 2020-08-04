99488c37f6ad4e1287aac00519b18ff34d2dc03c
welvin21 <wewe2712@connect.hku.hk>
2020-08-04 15:45:29 +0800

    Add link to homepage on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,29 @@
 import React, { FunctionComponent } from "react";
 import { Heading, Flex } from "@chakra-ui/core";
+import { Link } from "gatsby";
 
 import { Layout } from "../components";
 
-const NotFoundPage: FunctionComponent = () => (
-  <Layout>
-    <Flex
-      backgroundColor="primary"
-      h="100vh"
-      direction="column"
-      justify="center"
-      align="center"
-    >
-      <Heading color="text" as="h1" size="xl">
-        You just hit the route that doesn't exist.
-      </Heading>
-    </Flex>
-  </Layout>
-);
+const NotFoundPage: FunctionComponent = () => {
+  return (
+    <Layout>
+      <Flex
+        backgroundColor="primary"
+        h="100vh"
+        direction="column"
+        justify="center"
+        align="center"
+        textAlign="center"
+      >
+        <Heading color="text" as="h3" size="xl" my={3}>
+          We can't seem to find the page you are looking for.
+        </Heading>
+        <Link color="text" to="/">
+          go back to homepage
+        </Link>
+      </Flex>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
