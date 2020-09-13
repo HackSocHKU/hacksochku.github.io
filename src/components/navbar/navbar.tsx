1440f3dd40ddb7c6489e18aba7a0ea84afb531b1
welvin21 <wewe2712@connect.hku.hk>
2020-09-13 23:11:58 +0800

    Make navbar responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,32 +1,12 @@
 import React, { FunctionComponent } from "react";
-import { Flex, Text, Image } from "@chakra-ui/core";
+import { Flex, Text, Image, Box, useDisclosure } from "@chakra-ui/core";
 import { Link } from "gatsby";
+import { FiMenu } from "react-icons/fi";
 
+import { Mobile as MobileHeader } from "./mobile";
 import { NavItem } from "./navitem";
+import { routes } from "./routes";
 import hackSocLogo from "../../assets/images/hackSocLogo.png";
-
-const routes = [
-  {
-    id: "home",
-    text: "Home",
-  },
-  {
-    id: "about",
-    text: "About",
-  },
-  {
-    id: "sponsors",
-    text: "Sponsors",
-  },
-  {
-    id: "team",
-    text: "Team",
-  },
-  {
-    id: "contact",
-    text: "Contact",
-  },
-];
 
 interface NavBarComponentProps {
   activeHash: string;
@@ -36,38 +16,54 @@ interface NavBarComponentProps {
 export const NavBar: FunctionComponent<NavBarComponentProps> = ({
   activeHash,
   setActiveHash,
-}) => (
-  <Flex
-    as="nav"
-    align="center"
-    p={[2, 2, 4, 4]}
-    w="100vw"
-    position="fixed"
-    top={0}
-    backgroundColor="secondary"
-    opacity={0.8}
-    zIndex={1}
-  >
-    <Flex flex={1}>
-      <Link to="">
-        <Image
-          src={hackSocLogo}
-          w={[10, 12, 16, 16]}
-          h={[10, 12, 16, 16]}
-          mx={[5, 5, 20, 20]}
-        />
-      </Link>
+}) => {
+  const disclosure = useDisclosure();
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      p={[2, 2, 4, 4]}
+      w="100vw"
+      position="fixed"
+      top={0}
+      backgroundColor="secondary"
+      opacity={0.8}
+      zIndex={1}
+    >
+      <Flex flex={1}>
+        <Link to="">
+          <Image
+            src={hackSocLogo}
+            w={[10, 12, 16, 16]}
+            h={[10, 12, 16, 16]}
+            mx={[5, 5, 20, 20]}
+          />
+        </Link>
+      </Flex>
+      <Flex display={["none", "flex", "flex", "flex"]}>
+        {routes.map(({ id, text }) => (
+          <NavItem
+            key={id}
+            id={id}
+            text={text}
+            activeHash={activeHash}
+            setActiveHash={setActiveHash}
+          ></NavItem>
+        ))}
+      </Flex>
+      <Box
+        as={FiMenu}
+        size={30}
+        cursor="pointer"
+        display={["flex", "none", "none", "none"]}
+        onClick={disclosure.onOpen}
+      />
+      <MobileHeader
+        disclosure={disclosure}
+        activeHash={activeHash}
+        setActiveHash={setActiveHash}
+      />
     </Flex>
-    <Flex>
-      {routes.map(({ id, text }) => (
-        <NavItem
-          key={id}
-          id={id}
-          text={text}
-          activeHash={activeHash}
-          setActiveHash={setActiveHash}
-        ></NavItem>
-      ))}
-    </Flex>
-  </Flex>
-);
+  );
+};
